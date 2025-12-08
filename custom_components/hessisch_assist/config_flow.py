76786a87a7333602006a1d8e5ea29bde6ee1ca45
binbashmedium <48994110+binbashmedium@users.noreply.github.com>
2025-12-08 19:04:50 +0100

    Refactor Hessisch Assist config flow class

--- custom_components/hessisch_assist/config_flow.py
+++ custom_components/hessisch_assist/config_flow.py
@@ -1,15 +1,11 @@
 from homeassistant import config_entries
-from . import DOMAIN
 
-class HessischAssistConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle setup."""
+DOMAIN = "conversation_hessisch"
 
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
-    async def async_step_user(self, user_input=None):
-        """User initiated setup."""
-        if user_input is not None:
-            return self.async_create_entry(title="Hessisch Assist", data={})
-
-        return self.async_show_form(step_id="user")
+    async def async_step_user(self, info=None):
+        return self.async_create_entry(title="Hessisch Assist Agent", data={})
         
